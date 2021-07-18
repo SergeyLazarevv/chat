53e12be747b36f731d94232c5cbc428ce485ac5f
lazarev.sa <lazarev.sa@vitaexpress.ru>
2021-07-18 06:49:44 +0400

    sockets add

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,112 +2,54 @@
 import  express from 'express'
 import { Express } from 'express'
 import userRouter from './routes/userRoute'
-const WebSocket = require('ws')
+import socketService from './service/socketService'
+import { v4 as uuidv4 } from 'uuid';
+
 const http = require('http');
 
 const port = 8080;
 const host = '0.0.0.0';
 const app : Express = express();
-const wss = new WebSocket.Server({ noServer: true })
+
 
 
 app.use("/user",userRouter)
 
-app.use( (req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next();
-});
+// app.use( (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+//   //res.header('Access-Control-Allow-Origin', '*')
+//   res.header("Access-Control-Allow-Credentials", "true")
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next();
+// });
 
 
 // Массив со всеми подключениями
-
 const connections: any = [];
 console.log('init connection arr',connections.length)
+ 
+const server = http.createServer();
 
-function accept(req: any, res: any) {
-   // все входящие запросы должны использовать websockets
-   if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
-     res.end();
-     return;
-   }
- 
-   // может быть заголовок Connection: keep-alive, Upgrade
-   if (!req.headers.connection.match(/\bupgrade\b/i)) {
-     res.end();
-     return;
-   }
-   //console.log('wss',wss)
-   wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
-   console.log('clients',wss.clients)
-}
- 
- function onConnect(ws: any) {
-  connections.push('user')
-  console.log(connections)
-   //console.log('ws',ws)
-   ws.on('message', function (message: any) {
-     let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Гость";
-     ws.send(`Привет с сервера, ${name}!`);
- 
-     //setTimeout(() => ws.send("2021 hello!"), 35000);
+const io = require('socket.io')(server, {
+  allowEIO3: true,
+  cors: {
+    credentials: true,
+    origin: ['http://localhost:3000','*'],
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (client: any) => {
+  console.log('client',client)
+  client.on('event', (data: any) => { 
+    console.log('data')
    });
- }
- //console.log(module)
- if (!module.parent) {
-   http.createServer(accept).listen(port);
- } else {
-   exports.accept = accept;
-}
+  client.on('disconnect', (err: any) => { 
+    console.log('diss', err)
+   });
+});
+
+server.listen(8080);
 
 console.log(`running on http://${host}:${port}`);
-
-
-
-
-// 'use strict';
-// import  express from 'express'
-// import { Express } from 'express'
-// import userRouter from './routes/userRoute'
-// //const WebSocket = require('ws')
-// import socketService from './service/socketService'
-// const http = require('http');
-
-// const port = 8080;
-// const host = '0.0.0.0';
-// const app : Express = express();
-// //const wss = new WebSocket.Server({ noServer: true })
-
-// app.use("/user",userRouter)
-
-// // Массив со всеми подключениями
-// //const connections: any = [];
-
-
-// function accept(req: any, res: any) {
-//    // все входящие запросы должны использовать websockets
-//    if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
-//      res.end();
-//      return;
-//    }
- 
-//    // может быть заголовок Connection: keep-alive, Upgrade
-//    if (!req.headers.connection.match(/\bupgrade\b/i)) {
-//      res.end();
-//      return;
-//    }
- 
-//    const socket = new socketService(req, req.socket, Buffer.alloc(0));
-//    setTimeout(() => socket.send(), 35000);
-// }
- 
-//  if (!module.parent) {
-//    http.createServer(accept).listen(port);
-//  } else {
-//    exports.accept = accept;
-// }
-
-// console.log(`running on http://${host}:${port}`);
